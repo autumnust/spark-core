73542ebbbec13f848287981da3f4c63fb43ab5ea
autumnust <autumnust@gmail.com>
2016-10-27 21:02:26 -0400

    experiement code

---
+++ src/main/scala/org/apache/spark/rdd/UnionRDD.scala
@@ -0,0 +1,121 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.rdd
+
+import java.io.{IOException, ObjectOutputStream}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.reflect.ClassTag
+
+import org.apache.spark.{Dependency, Partition, RangeDependency, SparkContext, TaskContext}
+import org.apache.spark.annotation.DeveloperApi
+import org.apache.spark.util.Utils
+
+import org.apache.log4j.{Level, LogManager, PropertyConfigurator}
+
+/**
+ * Partition for UnionRDD.
+ *
+ * @param idx index of the partition
+ * @param rdd the parent RDD this partition refers to
+ * @param parentRddIndex index of the parent RDD this partition refers to
+ * @param parentRddPartitionIndex index of the partition within the parent RDD
+ *                                this partition refers to
+ */
+private[spark] class UnionPartition[T: ClassTag](
+    idx: Int,
+    @transient private val rdd: RDD[T],
+    val parentRddIndex: Int,
+    @transient private val parentRddPartitionIndex: Int)
+  extends Partition {
+
+  // parentRddIndex is helping find the parent RDD.
+  // parentRddPartitionIndex is helping finding the partition within the targeted parent RDD.
+  var parentPartition: Partition = rdd.partitions(parentRddPartitionIndex)
+
+  def preferredLocations(): Seq[String] = rdd.preferredLocations(parentPartition)
+
+  override val index: Int = idx
+
+  @throws(classOf[IOException])
+  private def writeObject(oos: ObjectOutputStream): Unit = Utils.tryOrIOException {
+    // Update the reference to parent split at the time of task serialization
+    parentPartition = rdd.partitions(parentRddPartitionIndex)
+    oos.defaultWriteObject()
+  }
+}
+
+@DeveloperApi
+class UnionRDD[T: ClassTag](
+    sc: SparkContext,
+    var rdds: Seq[RDD[T]])
+  extends RDD[T](sc, Nil) {  // Nil since we implement getDependencies
+
+  override def getPartitions: Array[Partition] = {
+    val array = new Array[Partition](rdds.map(_.partitions.length).sum)
+    var pos = 0
+    for ((rdd, rddIndex) <- rdds.zipWithIndex; split <- rdd.partitions) {
+      array(pos) = new UnionPartition(pos, rdd, rddIndex, split.index)
+      pos += 1
+    }
+    array
+  }
+
+  override def getDependencies: Seq[Dependency[_]] = {
+    val deps = new ArrayBuffer[Dependency[_]]
+    var pos = 0
+    for (rdd <- rdds) {
+      deps += new RangeDependency(rdd, 0, pos, rdd.partitions.length)
+      pos += rdd.partitions.length
+    }
+    deps
+  }
+
+  override def compute(s: Partition, context: TaskContext): Iterator[T] = {
+    val my_log = org.apache.log4j.LogManager.getLogger("myLogger")
+    my_log.setLevel(Level.INFO)
+    val printedBefore =
+      "[UnionRDD]StartAt:" + System.nanoTime().toString + "," +
+        "TaskAttempt ID:" + context.taskAttemptId().toString() +
+        ",Partition Id:" + context.partitionId().toString +
+        ",Stage Id:" + context.stageId().toString
+    my_log.info(printedBefore)
+    val startTime = System.nanoTime()
+
+    val part = s.asInstanceOf[UnionPartition[T]]
+    val funcRet = parent[T](part.parentRddIndex).iterator(part.parentPartition, context)
+
+    val endTime = System.nanoTime()
+    val printedAfter =
+      "[UnionRDD]EndAt:" + endTime.toString + "," +
+        "TaskAttempt ID:" + context.taskAttemptId().toString() +
+        ",Partition Id:" + context.partitionId().toString +
+        ",Stage Id:" + context.stageId().toString
+    my_log.info(printedAfter) ;
+
+    funcRet
+  }
+
+  override def getPreferredLocations(s: Partition): Seq[String] =
+    s.asInstanceOf[UnionPartition[T]].preferredLocations()
+
+  override def clearDependencies() {
+    super.clearDependencies()
+    rdds = null
+  }
+}
